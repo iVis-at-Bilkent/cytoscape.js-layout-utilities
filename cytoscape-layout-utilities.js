116f5e5df74a0c417650ba4f02c713fb32f95b7d
rumeysa ö <rumeysaozaydin@hotmail.com>
2019-06-27 14:07:52 +0300

    bug fix

--- cytoscape-layout-utilities.js
+++ cytoscape-layout-utilities.js
@@ -117,49 +117,6 @@ cytoscape('core', 'layoutUtilities', function (opts) {
 
     // set the instance on the scratch pad
     getScratch(cy).instance = instance;
-
-    var shiftKeyDown = false;
-    document.addEventListener('keydown', function (event) {
-      if (event.key == "Shift") {
-        shiftKeyDown = true;
-      }
-    });
-    document.addEventListener('keyup', function (event) {
-      if (event.key == "Shift") {
-        shiftKeyDown = false;
-      }
-    });
-    //Select the desired neighbors after taphold-and-free
-    cy.on('taphold', 'node', function (event) {
-      var target = event.target || event.cyTarget;
-      var tapheld = false;
-      var neighborhood;
-      var timeout = setTimeout(function () {
-        if (shiftKeyDown) {
-          cy.elements().unselect();
-          neighborhood = options.neighbor(target);
-          if (neighborhood) neighborhood.select();
-          target.lock();
-          tapheld = true;
-        }
-      }, options.neighborSelectTime - 500);
-      cy.on('free', 'node', function () {
-        var targetTapheld = event.target || event.cyTarget;
-        if (target == targetTapheld && tapheld === true) {
-          tapheld = false;
-          if (neighborhood) neighborhood.select();
-          target.unlock();
-        } else {
-          clearTimeout(timeout);
-        }
-      });
-      cy.on('drag', 'node', function () {
-        var targetDragged = event.target || event.cyTarget;
-        if (target == targetDragged && tapheld === false) {
-          clearTimeout(timeout);
-        }
-      });
-    });
   }
 
   // return the instance of extension
