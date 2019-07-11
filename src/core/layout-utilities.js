94c0a7a17a06185c73aa6a42002297c49fcd18b4
rumeysa ö <rumeysaozaydin@hotmail.com>
2019-07-11 16:18:30 +0300

    method for placing new nodes

--- src/core/layout-utilities.js
+++ src/core/layout-utilities.js
@@ -1,12 +1,4 @@
 
-    
-// class Ploynomino {
-//   constructor(name, level) {
-//       this.name = name;
-//       this.level = level;
-//   }
-// }
-
 
 var layoutUtilities = function (cy, options) {
 
@@ -24,7 +16,7 @@ var layoutUtilities = function (cy, options) {
     });
   };
 
-  instance.placeNewNodes = function (currentNodes, eles){
+  instance.placeNewNodes = function (eles){
     var disconnectedNodes = [];
     eles.forEach(function (ele){
       var neighbors = ele.neighborhood().nodes(":visible");
@@ -35,22 +27,21 @@ var layoutUtilities = function (cy, options) {
         instance.nodeWithOneNeighbor(neighbors[0], ele);
       }
       else{
-        disconnectedNodes.push(ele)
+        disconnectedNodes.push(ele);
       }
     });
     if(disconnectedNodes.length >= 1){
-      console.log("leleleele");
-      instance.disconnectedNodes(currentNodes, disconnectedNodes);
+      instance.disconnectedNodes(disconnectedNodes);
     }
-  }
+  };
 
-  instance.disconnectedNodes = function(currentNodes, newEles){
+  instance.disconnectedNodes = function (newEles){
     var leftX = Number.MAX_VALUE;
     var rightX = Number.MIN_VALUE;
     var topY = Number.MAX_VALUE;
     var bottomY = Number.MIN_VALUE;
     // Check the x and y limits of all hidden elements and store them in the variables above
-    currentNodes.forEach(function(node){
+    cy.nodes(':visible').forEach(function(node){
       var halfWidth = node.outerWidth()/2;
       var halfHeight = node.outerHeight()/2;
       if (node.position("x") - halfWidth < leftX)
@@ -63,23 +54,124 @@ var layoutUtilities = function (cy, options) {
         bottomY = node.position("y") + halfHeight;
     });
  
-    //var numOfPieces = eles.length;
-    //var angle = 360 / len;
     var radiusy = topY - bottomY;
     var radiusx = rightX - leftX;
-    var radius = Math.sqrt(radiusx * radiusx + radiusy * radiusy);
+    var innerRadius = (Math.sqrt(radiusx * radiusx + radiusy * radiusy)) / 2;
+    var outerRadius = innerRadius + options.offset;
     var centerX = (leftX + rightX)/2;
     var centerY = (topY + bottomY)/2;
-    
-    newEles.forEach(function(newEle){
-      console.log("insideforeach")
-      var offsetX = instance.generateRandom(radius/2, radius/2 + options.offset, 0);
-      var offsetY = instance.generateRandom(radius/2, radius/2 + options.offset, 0);
-      newEle.position("x", centerX + offsetX);
-      newEle.position("y", centerY + offsetY);
+    var components = this.findComponents(newEles);
+    var numOfComponents = components.length;
+    var angle = 360/numOfComponents;
+    var count = 1;
+
+
+    components.forEach(function(component){
+      // var horizontalParam = instance.generateRandom(200, 250 , 0);
+      // var verticalParam = instance.generateRandom(200, 250, 0);
+      // var newCenterX = centerX  + horizontalParam;
+      // var newCenterY = centerY + verticalParam;
+
+      var distFromCenter = innerRadius + Math.random() * (outerRadius - innerRadius);
+      var curAngle = angle * count;
+      var x = distFromCenter * Math.cos(curAngle);
+      var y = distFromCenter * Math.sin(curAngle);
+
+      if(component.length == 1){
+        component[0].position("x", x);
+        component[0].position("y", y);
+      }
+      else{
+        var max = 0;
+        var index = 0;
+        var positioned = [];
+        for(var i=0; i<component.length; i++){
+          positioned.push(false);
+          var neighbors = component[i].neighborhood().nodes();
+          if(neighbors.length > max){
+            max = neighbors.length;
+            index = i;
+          }
+        }
+
+        positioned[index] = true;
+        component[index].position("x", x);
+        component[index].position("y", y);
+          
+        for(var i=0; i<component.length; i++){
+          if(i != index){
+            var neighbors = component[i].neighborhood().nodes();
+            var positionedNeigbors = [];
+            for(var j =0; j< neighbors.length; j++){
+              if(positioned[component.indexOf(neighbors[j])]){
+                positionedNeigbors.push(neighbors[j]);
+              }
+            }
+            if (positionedNeigbors.length > 1) {
+              instance.nodeWithMultipleNeighbors(component[i], positionedNeigbors);
+            } else if(positionedNeigbors.length == 1) instance.nodeWithOneNeighbor(positionedNeigbors[0], component[i]);
+            else{
+              var horizontalP = instance.generateRandom(20, 60 , 0);
+              var verticalP = instance.generateRandom(20, 60, 0);
+              component[i].position("x", x + horizontalP);
+              component[i].position("y", y + verticalP);
+            }
+          }
+          positioned[i] = true;
+        }
+      }
+      count++;
     });
-    
   };
+
+  instance.findComponents = function(newEles){
+    
+    var adjListArray = [];
+
+    newEles.forEach(function(ele){
+      var neighbors = ele.neighborhood().nodes();
+      var listOfIndexes = [];
+      neighbors.forEach(function(neigbor){
+        var index = newEles.indexOf(neigbor);
+        listOfIndexes.push(index);
+      });
+      adjListArray.push(listOfIndexes);
+    });
+
+    // Mark all the vertices as not visited 
+    var visited = []; 
+    for(var v = 0; v < newEles.length; v++){
+        visited.push(false); 
+    }
+
+    var listOfComponents = [];
+    
+  
+    for (var v=0; v<newEles.length; v++) 
+    { 
+        var elesOfComponent = [];
+        if (visited[v] == false) 
+        { 
+            // print all reachable vertices 
+            // from v 
+            this.DFSUtil(v, visited, adjListArray, newEles, elesOfComponent); 
+            listOfComponents.push(elesOfComponent);
+        } 
+    }
+    
+    return listOfComponents;
+  };
+
+  instance.DFSUtil = function(v, visited, adjListArray, newEles, elesOfComponent){
+    // Mark the current node as visited and print it 
+    visited[v] = true; 
+    elesOfComponent.push(newEles[v]);
+    // Recur for all the vertices 
+    // adjacent to this vertex 
+    for (var i=0; i<adjListArray[v].length ; i++) { 
+      if(!visited[adjListArray[v][i]]) this.DFSUtil(adjListArray[v][i], visited, adjListArray, newEles, elesOfComponent); 
+    } 
+  }; 
 
   instance.nodeWithOneNeighbor = function(mainEle, hiddenEle) {
     var quadrants = instance.checkOccupiedQuadrants(mainEle, hiddenEle);
@@ -154,8 +246,11 @@ var layoutUtilities = function (cy, options) {
     hiddenEle.position("y",newCenterY);
   };
 
-  instance.nodeWithMultipleNeighbors = function(ele){
-    var neighbors = ele.neighborhood().nodes(":visible");
+  instance.nodeWithMultipleNeighbors = function(ele, neighbors){
+    ///var neighbors = ele.neighborhood().nodes(":visible");
+    if(neighbors == null){
+      var neighbors = ele.neighborhood().nodes(":visible");
+    }
       var x = 0;
       var y = 0;
       var count = 0;
@@ -199,18 +294,7 @@ var layoutUtilities = function (cy, options) {
     return occupiedQuadrants;
   };
 
-  // instance.layoutComponents= function(listOfEles){
-  //   for(var i = 0; i < listOfEles.length ; i++){
-  //     var disconnectedPolys;
-  //     for(var j = 0; j < listOfEles[i].length; j++){
-      
-  //     }
-  //   }
-  // };
-
-  // return the instance
   return instance;
 };
 
 module.exports = layoutUtilities;
-
